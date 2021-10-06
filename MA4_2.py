c4c2a9488a827ad94219db83416d3b678be7fa98
Aleksander Andrzejewski <alan7585@atterbom.it.uu.se>
2021-10-06 17:53:12 +0200

    files after doing on Linux

--- MA4_2.py
+++ MA4_2.py
@@ -1,10 +1,13 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Mon Oct  4 15:27:08 2021
 
 @author: aleks
 """
-from numba import njit
+import matplotlib
+matplotlib.use('Agg')
+#from numba import njit
 import matplotlib.pyplot as plt
 from time import perf_counter as pc
 import numpy as np
@@ -15,12 +18,12 @@ def fib_pure_py(n):
         return n
     else:
         return(fib_pure_py(n-1) + fib_pure_py(n-2))
-@njit
-def fib_numba_py(n):
-    if n <= 1:
-        return n
-    else:
-        return(fib_numba_py(n-1) + fib_numba_py(n-2))
+#@njit
+#def fib_numba_py(n):
+   # if n <= 1:
+    #    return n
+   # else:
+    #    return(fib_numba_py(n-1) + fib_numba_py(n-2))
     
 def task_4():
     (fig, ax) = plt.subplots()
@@ -35,10 +38,10 @@ def task_4():
         time_python.append(end_p - start_p)
         
         #numba
-        start_n = pc()
-        fib_pure_py(i)
-        end_n = pc()
-        time_numba.append(end_n - start_n)
+       # start_n = pc()
+       # fib_pure_py(i)
+       # end_n = pc()
+       # time_numba.append(end_n - start_n)
         
         #c++
         start_n = pc()
@@ -47,15 +50,15 @@ def task_4():
         end_n = pc()
         time_cpp.append(end_n - start_n)
         
-    ax.plot(np.linspace(30, 45, num=15), time_python)
-    ax.plot(np.linspace(30,45, num = 15), time_numba)
-    ax.plot(np.linspace(30,45, num = 15), time_cpp)
+    ax.plot(np.linspace(30, 45, num=16), time_python)
+   # ax.plot(np.linspace(30,45, num = 15), time_numba)
+    ax.plot(np.linspace(30,45, num = 16), time_cpp)
     ax.set_ylabel("Time [s]")
     plt.savefig("cpp_numba_python.png")
     
 def task_5():   
     time_pure_python = []
-    time_numba = []
+   # time_numba = []
     #f = Integer(5)
     (fig, ax) = plt.subplots()
     #print(f.fib())
@@ -63,20 +66,20 @@ def task_5():
         start = pc()
         fib_pure_py(i+1)
         end = pc()
-        start_numba = pc()    
-        fib_numba_py(i+1)
-        end_numba = pc()
+       # start_numba = pc()    
+       # fib_numba_py(i+1)
+       # end_numba = pc()
         time_pure_python.append(end - start)
-        time_numba.append(end_numba - start_numba)
+       # time_numba.append(end_numba - start_numba)
     ax.plot(np.linspace(1, 30, num=30), time_pure_python)
-    ax.plot(np.linspace(1,30, num = 30), time_numba)
+   # ax.plot(np.linspace(1,30, num = 30), time_numba)
     ax.set_ylabel("Time [s]")
     plt.savefig("numba_vs_python.png")
     
 def task_6():
     f = Integer(47)
     print(f.fib())
-    print(fib_numba_py(47))
+   # print(fib_numba_py(47))
     
 def main():
     task_4()
